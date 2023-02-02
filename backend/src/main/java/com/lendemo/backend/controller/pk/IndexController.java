d762dcbaadc3c5a3d75d08170f259852ef3761cb
hxc <cei196505153@163.com>
2023-02-02 19:43:02 +0800

    项目骨架搭建1

---
+++ backend/src/main/java/com/lendemo/backend/controller/pk/IndexController.java
@@ -0,0 +1,20 @@
+package com.lendemo.backend.controller.pk;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class IndexController {
+
+    @RequestMapping("/pk/getbotinfo/")
+    public Map<String, String> getBotInfo() {
+        Map<String, String> bot1 = new HashMap<>();
+        bot1.put("name", "tiger");
+        bot1.put("rating", "1500");
+
+        return bot1;
+    }
+}
