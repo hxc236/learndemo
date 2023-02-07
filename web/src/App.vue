abb57a993e733ea55739c87e9531c914333cefbb
hxc <cei196505153@163.com>
2023-02-07 13:48:53 +0800

    后端实现用户信息和注册两个API

--- web/src/App.vue
+++ web/src/App.vue
@@ -29,6 +29,36 @@ export default {
       console.log(resp);
     }
   });
+
+  $.ajax({
+    url: "http://127.0.0.1:1644/user/account/info/",
+    type: "get",
+    headers: {
+      Authorization: "Bearer " + "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI1YjBhMTJmZDhlMWM0NGUxOGIyMGE1NjYwNWM2ODMxOCIsInN1YiI6IjEiLCJpc3MiOiJzZyIsImlhdCI6MTY3NTc0NTA5NiwiZXhwIjoxNjc2OTU0Njk2fQ.CQpxnoVaSkWV5XMUv5q6vd8_mtt0ceUs1eAYc0wbQI0"
+    },
+    success(resp) {
+      console.log(resp);
+    },
+    error(resp) {
+      console.log(resp);
+    }
+  });
+
+  $.ajax({
+      url: "http://127.0.0.1:1644/user/account/register/",
+      type: "post",
+      data: {
+        username: "hxc",
+        password: "123",
+        confirmedPassword: "123"
+      },
+      success(resp) {
+      console.log(resp);
+      },
+      error(resp) {
+        console.log(resp);
+      }
+    })
   }
 }
 </script>
